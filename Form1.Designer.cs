00670f02aaba5a88628b9e33d5c7be2be9b049b8
J.J.Ding <louis7012161@gmail.com>
2021-04-08 21:47:16 +0800

    Signed-off-by: J.J.Ding <louis7012161@gmail.com> Start program_

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Log_Analysis
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_fileAdress = new System.Windows.Forms.TextBox();
+            this.btn_OpenFile = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_fileAdress
+            // 
+            this.txt_fileAdress.Location = new System.Drawing.Point(17, 16);
+            this.txt_fileAdress.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_fileAdress.Name = "txt_fileAdress";
+            this.txt_fileAdress.Size = new System.Drawing.Size(668, 25);
+            this.txt_fileAdress.TabIndex = 0;
+            // 
+            // btn_OpenFile
+            // 
+            this.btn_OpenFile.Location = new System.Drawing.Point(716, 14);
+            this.btn_OpenFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_OpenFile.Name = "btn_OpenFile";
+            this.btn_OpenFile.Size = new System.Drawing.Size(100, 29);
+            this.btn_OpenFile.TabIndex = 1;
+            this.btn_OpenFile.Text = "開啟舊檔";
+            this.btn_OpenFile.UseVisualStyleBackColor = true;
+            this.btn_OpenFile.Click += new System.EventHandler(this.btn_OpenFile_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 65);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(799, 482);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_OpenFile);
+            this.Controls.Add(this.txt_fileAdress);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_fileAdress;
+        private System.Windows.Forms.Button btn_OpenFile;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Log_Analysis
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog odf = new OpenFileDialog();
+        String fileAdress;
+        string line;
+        public Form1()
+        {
+            InitializeComponent();
+            odf.AddExtension = true;
+            odf.CheckFileExists = true;
+            odf.Filter = "csv檔|*.csv|純文字檔|*.txt|所有檔案|*.*";
+            odf.FilterIndex = 0;
+            odf.FileName = String.Empty;
+            odf.InitialDirectory = @"C:\";
+            odf.Multiselect = false;
+            odf.RestoreDirectory = true;
+
+            odf.ShowReadOnly = true;
+            odf.Title = string.Empty;
+            odf.FileOk += new CancelEventHandler(odf_FileOk);
+        }
+
+        private void btn_OpenFile_Click(object sender, EventArgs e)
+        {
+            //StreamReader log_file = new StreamReader("C:\\Users\\User\\Desktop\\v2k.txt");
+            odf.ShowDialog();
+
+        }
+
+        private void odf_FileOk(object sender, CancelEventArgs e)
+        {
+            //MessageBox.Show(odf.FileName);
+            txt_fileAdress.Text = odf.FileName;
+            FileStream file = new FileStream(odf.FileName, FileMode.Open, FileAccess.Read, FileShare.None);
+            StreamReader sr = new StreamReader(file, System.Text.Encoding.GetEncoding("UTF-8"));
+            line = sr.ReadLine();
+            MessageBox.Show(line);
+
+        }
+    }
+}
